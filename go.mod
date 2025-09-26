04a09a2409e696774e0bb08421f4f0206a933e92
Shubham <shubham16290@gmail.com>
2025-09-26 22:34:26 +0530

    project-setup

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/gitshubham45/auctionSimulator
+
+go 1.25.0
---
+++ internal/auction/types.go
@@ -0,0 +1,22 @@
+package auction
+
+import "time"
+
+type Attribute map[string]string
+
+type Bid struct {
+	BidderId   int       `json:"bidder_id"`
+	Amount     float64   `json:"amount"`
+	ReceivedAt time.Time `json:"received_at"`
+}
+
+type Auction struct {
+	ID         int       `json:"id"`
+	Attributes Attribute `json:"attributes"`
+	TimeoutSec int       `json:"timeout_sec"`
+	StartTs    time.Time `json:"start_ts"`
+	EndTs      time.Time `json:"end_ts"`
+	Bids       []Bid     `json:"bids"`
+	Winner     *Bid      `josn:"winner"`
+	DurationMs int64     `json:"duration_ms"`
+}
